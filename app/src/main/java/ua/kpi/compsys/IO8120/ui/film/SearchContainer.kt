b2b9ca311250853eedc848d01ff4f3385bd54c97
Aleks Pavlovets <aleks.pavlovets@gmail.com>
2021-05-20 01:43:32 +0300

    lab5

--- app/src/main/java/ua/kpi/compsys/IO8120/ui/film/SearchContainer.kt
+++
@@ -1,9 +0,0 @@
-package ua.kpi.compsys.IO8120.ui.film
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-class SearchContainer {
-    @JsonProperty("Search")
-    var search: MutableList<Search> = ArrayList()
-}
-
