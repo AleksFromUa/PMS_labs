9bfea3d666404eb3dfcac34dd89ed515a9c3318a
Aleks Pavlovets <aleks.pavlovets@gmail.com>
2021-05-22 03:00:12 +0300

    test with git rm

--- feature-gallery/src/main/java/ua/kpi/comsys/IO8120/feature_gallery/ui/GalleryViewModel.kt
+++
@@ -1,8 +0,0 @@
-package ua.kpi.comsys.IO8120.feature_gallery.ui
-
-import android.net.Uri
-import androidx.lifecycle.ViewModel
-
-internal class GalleryViewModel : ViewModel() {
-    val photos = mutableListOf<Uri>()
-}
